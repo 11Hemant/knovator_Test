72a38c17bd960a6809e98f1eee16fba921b7c030
Hemant Khandave <121818826+11Hemant@users.noreply.github.com>
2023-10-11 13:02:03 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,47 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "us-east-2" 
+}
+
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "Hemant" 
+}
+
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.medium" 
+}
+
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "jenkins-sgroup-dec-2021" 
+}
+
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "my-ec2-instance" 
+} 
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-0430580de6244e02e" 
+}
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
+
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
